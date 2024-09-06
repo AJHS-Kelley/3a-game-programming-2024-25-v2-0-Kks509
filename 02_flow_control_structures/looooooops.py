0216d72a2ffac11fab771f3f1f2293b2cdbd61c0
Kks509 <s12134573@students.duvalschools.org>
2024-09-06 12:59:20 -0400

    added playerscore

--- 02_flow_control_structures/looooooops.py
+++ 02_flow_control_structures/looooooops.py
@@ -1,4 +1,6 @@
 # Loops, Kelston Spaulding, v0.0
+import random # import the random module for us to use.
+# Generally put all your import statements at the top.
 
 # TWO TYPES OF LOOPS
 # for <-- Used when you know how many loops you'll need.
@@ -32,3 +34,22 @@ for eachfruit in fruits:
 # for loops using range(). range(x) is EXCLUSIVE, it starts at 0 and ends at x - 1
 for i in range(10): # range is 0 - 9
     print(i)
+
+# Might not always want to start at zero
+for i in range(10, 100): #
+    print(i)
+
+# Not want to count by 1 -- RARE
+for i in range(10, 100, 5): # 10 = start, 100 - 1 = stop, 5 = # to count by
+    print(i)
+
+# Sometimes you're not done writing the loops.
+for x in range(10):
+    pass # tells Python this loops isn't finished, don't freak out.
+
+# while loops -- Musical Chairs
+playerScore = 0
+while playerScore < 100: # Run as long as this is true.
+    print(f"Starting: {playerScore}")
+    playerScore += random.randit(1, 3)
+    print(f"After: {playerScore}")
