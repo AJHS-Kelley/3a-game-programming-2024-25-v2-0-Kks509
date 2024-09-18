e1d426e9b8d04c597285d0ebc2b6cde70e1714a3
Kks509 <s12134573@students.duvalschools.org>
2024-09-18 13:17:00 -0400

    added choices

--- Rock_Paper_Scissors/Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.py
@@ -26,8 +26,8 @@ else:
     playerName = input("Please type your name and press enter.\n")
 
 # THE RULES using MULTI-LINE STRINGS
-print("""
-Welcome {testPlayer} to the Rock, Paper, Scissors Robot!
+print(f"""
+Welcome {playerName} to the Rock, Paper, Scissors Robot!
 It's Time To Play Rock, Paper, Scissors!
     
 You will play against the CPU. The first person to score 5 points wins.
@@ -38,3 +38,30 @@ The CPU will select ROCK, PAPER, or SCISSORS at random.
 2) SCISSORS BEATS PAPER
 3) PAPER BEATS ROCKS
 """)
+
+# MULTI-LINE STRINGS CAN BE USED AS BIG COMMENTS
+"""
+Anything in between the sets of double-quotes is just ignored.
+If you need to write large comments, it's easier to use multi-line strings than
+putting a # in frontof 15 different times
+"""
+
+# MAIN GAME LOOP
+while playerScore < 5 and cpuScore < 5:
+    print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n")
+    playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
+    if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
+        playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
+        if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
+            print("You are not following directions. Please try again.\n")
+            exit()
+        print(f"You have chosen {playerChoice}.\n")
+    else:
+        print(f"You have chosen {playerChoice}.\n")
+    
+
+
+    # let cpu select choice at random.
+    # compare player choice to cpu choice
+    # print the results to the screen
+    # award point to winner and output results.
