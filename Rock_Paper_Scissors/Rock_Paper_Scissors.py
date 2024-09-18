36ef3851083cec730efe49943360e82ff32a18af
Kks509 <s12134573@students.duvalschools.org>
2024-09-18 11:49:53 -0400

    added player and cpu score

---
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.py
@@ -0,0 +1,13 @@
+# Rock, Paper, Scissors by Kelston Spaulding, v0.1
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCTURES -- PLAYER
+playerScore = 0
+playerName = "Test Player"
+playerChoice = None
+
+# DATA STRUCTURES -- CPU
+cpuScore = 0
+cpuChoice = None
