ddd9844520d584209cca3bd5ea679406e4ed7c11
Kks509 <s12134573@students.duvalschools.org>
2024-09-18 12:49:27 -0400

    added rules

--- Rock_Paper_Scissors/Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.py
@@ -1,4 +1,4 @@
-# Rock, Paper, Scissors by Kelston Spaulding, v0.1
+# Rock, Paper, Scissors, by Kelston Spaulding, v0.1
 
 # MODULE IMPORTS
 import random
@@ -15,11 +15,26 @@ cpuChoice = None
 # PLAYER NAME INPUT
 playerName = input("Please type your name and press enter.\n")
 print(f"Hello{playerName}!\n")
-isCorrect = input("Is that correct? Type yes or no and press enter.\n").Lower()
+isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
 
-#.lower
+# .lower() can turn ALL input into lowercase.
+# .upper() can turn ALL input into uppercase.
 
 if isCorrect == "yes":
     print(f"ok {playerName}, let's play Rock, Paper, Scissors!\n")
 else:
     playerName = input("Please type your name and press enter.\n")
+
+# THE RULES using MULTI-LINE STRINGS
+print("""
+Welcome {testPlayer} to the Rock, Paper, Scissors Robot!
+It's Time To Play Rock, Paper, Scissors!
+    
+You will play against the CPU. The first person to score 5 points wins.
+You will select from ROCK, PAPER, SCISSORS.
+The CPU will select ROCK, PAPER, or SCISSORS at random.
+      
+1) ROCK BEATS SCISSORS
+2) SCISSORS BEATS PAPER
+3) PAPER BEATS ROCKS
+""")
