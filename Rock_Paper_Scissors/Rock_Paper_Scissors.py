7daf34ef8bd695916d10de845e409d98ff184e64
Kks509 <s12134573@students.duvalschools.org>
2024-09-18 12:04:17 -0400

    lower()

--- Rock_Paper_Scissors/Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.py
@@ -11,3 +11,15 @@ playerChoice = None
 # DATA STRUCTURES -- CPU
 cpuScore = 0
 cpuChoice = None
+
+# PLAYER NAME INPUT
+playerName = input("Please type your name and press enter.\n")
+print(f"Hello{playerName}!\n")
+isCorrect = input("Is that correct? Type yes or no and press enter.\n").Lower()
+
+#.lower
+
+if isCorrect == "yes":
+    print(f"ok {playerName}, let's play Rock, Paper, Scissors!\n")
+else:
+    playerName = input("Please type your name and press enter.\n")
