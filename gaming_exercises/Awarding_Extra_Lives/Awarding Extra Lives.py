6d57838cbd23819b930724b9e2c0b3ef9aa6a863
Kks509 <s12134573@students.duvalschools.org>
2024-09-24 13:18:49 -0400

    fixing

--- gaming_exercises/Awarding_Extra_Lives/Awarding Extra Lives.py
+++ gaming_exercises/Awarding_Extra_Lives/Awarding Extra Lives.py
@@ -26,10 +26,9 @@ elif score < 100001:
     print("Gain 1 life.\n")
     lives += 1
     # What code will actually change the number of lives stored in computer memory?
-else:   
+else score >= 100000:
     print("Gain 2 Lives.\n")
     lives += 2
     # What code will actually change the number of lives stored in computer memory?
-
 print(f"Your score is {score}! You now have {lives}. ")
 print(lives)
