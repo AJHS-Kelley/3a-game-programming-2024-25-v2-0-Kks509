e23a19469c3be259116107bf0cb1f72ca0780910
Kks509 <s12134573@students.duvalschools.org>
2024-09-30 13:19:43 -0400

    needs fixing

--- gaming_exercises/Rock_Paper_Scissors/rps_Performance.py
+++ gaming_exercises/Rock_Paper_Scissors/rps_Performance.py
@@ -13,7 +13,11 @@ cpuScore = 0
 cpuChoice = None
 
 # MAIN GAME LOOP
-while playerScore < 5 and cpuScore < 5:
+loopCount = 0
+loopsReq = int(input("How many loops do you want?\nEnter an integer, no commas, and press ENTER.\n"))
+# req is the universal abbreviation in computer programming for REQUEST. reqs = REQUESTS
+rpsTimeStart = time.time() # returns the number of seconds since Jan. 01, 1970 @ 12:00AM
+while loopCount < loopsReq:
     
     # let cpu select choice at random.
     cpuChoice = random.randint(0, 2) # randomly select 0, 1, or 2.
@@ -28,17 +32,18 @@ while playerScore < 5 and cpuScore < 5:
         exit()
     #print(f"CPU Choice: {cpuChoice}")
 
-    # let Player select choice at random.
+    # let PLAYER select choice at random.
     playerChoice = random.randint(0, 2) # randomly select 0, 1, or 2.
     if playerChoice == 0:
         playerChoice = "rock"
     elif playerChoice == 1:
         playerChoice ="scissors"
-    elif playerChoice ==2:
+    elif playerChoice == 2:
         playerChoice = "paper"
     else:
         print("Unable to determine CPU choice.\nPlease restart.\n")
-        exit()   
+        exit() 
+
     # compare player choice to cpu choice
     if playerChoice == "rock" and cpuChoice == "paper":
         # CPU WINS
@@ -54,6 +59,7 @@ while playerScore < 5 and cpuScore < 5:
         # DRAW
         print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
         print("It's a draw.\n")
+        numDraws += 1
     elif playerChoice == "scissors" and cpuChoice == "rock":
         # CPU WINS
         print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
@@ -68,6 +74,12 @@ while playerScore < 5 and cpuScore < 5:
         # DRAW
         print(f"The chose {cpuChoice} and you chose {playerChoice}.\n")
         print("It's a draw.\n")
+        numDraws += 1
+        # CPU WINS
+    elif playerChoice == "paper" and cpuChoice == "scissors":
+        print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+        print("The CPU wins a point.\n")
+        cpuScore += 1
     elif playerChoice == "paper" and cpuChoice == "rock":
         # PLAYER WINS
         print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
@@ -75,12 +87,18 @@ while playerScore < 5 and cpuScore < 5:
         playerScore += 1
     elif playerChoice == "paper" and cpuChoice == "paper":
         # DRAW
-         print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
-         print("It's a draw.\n")
-    elif playerChoice == "paper" and cpuChoice == "scissors":
-         print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
-         print("The CPU wins a point.\n")
-         cpuScore += 1
-    
+        print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+        print("It's a draw.\n")
+        numDraws += 1
+        # CPU WINS
+    print(f"Your Final Score: {playerScore}CPU Final Score: {cpuScore}.\n ")
+    if playerScore > cpuScore:
+        print(f"Congratulations. a winner is you!\n")
+    elif cpuScore > playerScore:
+        print(f"The CPU wins. You are a dissapointment to all.\n")
+    else:
+        print("Unable to determine a winner.\nPlease restart.\n")
+        exit()
+    loopCount +=1
 
     # award point to winner and output results.
