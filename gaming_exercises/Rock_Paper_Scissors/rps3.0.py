e4709841c28787b0d960190c16ae819e8568ee05
Kks509 <s12134573@students.duvalschools.org>
2024-10-16 13:03:05 -0400

    added choices

--- gaming_exercises/Rock_Paper_Scissors/rps3.0.py
+++ gaming_exercises/Rock_Paper_Scissors/rps3.0.py
@@ -17,42 +17,34 @@ def playerName(): # Function Signature -- name of function
     playerName = input("Please type your name and press enter.\n")
     print(f"Hello {playerName}!.\n")
     isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
-
-    # .lower() can turn ALL input into lowercase.
-    # .upper() can turn ALL input into uppercase.
-
     if isCorrect == "yes":
         print(f"ok {playerName}, let's play Rock, Paper, Scissors!\n")
     else:
         playerName = input("Please type your name and press enter.\n")
-    
+    return playerName
+
 # calling the function
-playerName()
+playerName = playerName()
 
 # THE RULES using MULTI-LINE STRINGS
-print(f"""
-Welcome {playerName} to the Rock, Paper, Scissors Robot!
-It's Time To Play Rock, Paper, Scissors!
+def rules():
+    print(f"""
+    Welcome {playerName} to the Rock, Paper, Scissors Robot!
+    It's Time To Play Rock, Paper, Scissors!
     
-You will play against the CPU. The first person to score 5 points wins.
-You will select from ROCK, PAPER, SCISSORS.
-The CPU will select ROCK, PAPER, or SCISSORS at random.
+    You will play against the CPU. The first person to score 5 points wins.
+    You will select from ROCK, PAPER, SCISSORS.
+    The CPU will select ROCK, PAPER, or SCISSORS at random.
       
-1) ROCK BEATS SCISSORS
-2) SCISSORS BEATS PAPER
-3) PAPER BEATS ROCKS
-""")
+    1) ROCK BEATS SCISSORS
+    2) SCISSORS BEATS PAPER
+    3) PAPER BEATS ROCKS
+    """)
+    # Does another part of this program need to access this information
+    # IF YES, YOU MUST HAVE A return STATEMENT
+    # IF NO, a return STATEMENT IS NOT REQUIRED
 
-# MULTI-LINE STRINGS CAN BE USED AS BIG COMMENTS
-"""
-Anything in between the sets of double-quotes is just ignored.
-If you need to write large comments, it's easier to use multi-line strings than
-putting a # in frontof 15 different times
-"""
-
-# MAIN GAME LOOP
-while playerScore < 5 and cpuScore < 5:
-    print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n")
+def playerChoice():
     playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
     if playerChoice != "rock" and playerChoice != "paper" and playerChoice != "scissors":
         playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
@@ -62,18 +54,28 @@ while playerScore < 5 and cpuScore < 5:
         print(f"You have chosen {playerChoice}.\n")
     else:
         print(f"You have chosen {playerChoice}.\n")
-    
-    # let cpu select choice at random.
+    return playerChoice
+
+def cpuChoice():
     cpuChoice = random.randint(0, 2) # randomly select 0, 1, or 2.
     if cpuChoice == 0:
         cpuChoice = "rock"
     elif cpuChoice == 1:
         cpuChoice ="scissors"
-    elif cpuChoice ==2:
+    elif cpuChoice == 2:
         cpuChoice = "paper"
     else:
         print("Unable to determine CPU choice.\nPlease restart.\n")
         exit()
+    return cpuChoice 
+
+# MAIN GAME LOOP
+while playerScore < 5 and cpuScore < 5:
+    print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n")
+    
+    
+    # let cpu select choice at random.
+   
     #print(f"CPU Choice: {cpuChoice}")
         
     # compare player choice to cpu choice
